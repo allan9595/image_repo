fb631aaea5f32d7cba6df16b8a3a73f0180b0535
*allan9595* <bohanzhang@my.unt.edu>
2020-01-27 00:48:17 -0500

    finish basic add to s3, working on deletion

--- routes/api/products.js
+++ routes/api/products.js
@@ -15,6 +15,19 @@ const productUpload = (req, res) => {
     
     //check if the file are valid ext from the req object
     if(req.fileValidationError) {
+        const directory = 'temp';
+
+        //remove all the files form the server temp folder if the ext not good
+        fs.readdir(directory, (err, files) => {
+            if (err) throw err;
+
+            for (const file of files) {
+                fs.unlink(path.join(directory, file), err => {
+                    if (err) throw err;
+                });
+            }
+        });
+        
         return res.status(400).json({"message":req.fileValidationError}).end();
     }
 
@@ -30,45 +43,67 @@ const productUpload = (req, res) => {
     //create a new s3 instance
     const s3 = new aws.S3();
 
-    FileType.fromStream(fs.createReadStream(req.file.path)) //append the file type to the end prevent to prevent bad intend
-        .then(file => {
+    let uploadData = [];
+    let errors = [];
+    let errorsS3 = [];
+    
+    req.files.map(fileSingle => {
+
+        FileType.fromStream(fs.createReadStream(fileSingle.path)) //append the file type to the end 
+            .then(file => {
+
             //ACL: set the bucket as public readable
             //Bucket: the bucket name
             //body: open the file as a readable stream and set it to the Body
             //Key: the file name
+            
             const params = {
                 ACL: 'public-read',
                 Bucket: process.env.BUCKET_NAME,
-                Body: fs.createReadStream(req.file.path),
+                Body: fs.createReadStream(fileSingle.path),
                 Key: `products/${uuidv4()}.${file.ext}`,
-                ContentType: req.file.mimetype,
+                ContentType: fileSingle.mimetype,
                 ContentDisposition: "inline"
             };
-            console.log(req.file);
+            
             //the actual s3 upload 
             s3.upload(params, (err, data) => {
                 if (err) {
-                    console.log('Error occured while trying to upload to S3 bucket', err);
+                    errorsS3.push(err);
+                    //return a message to client if save to s3 failed
+                    if(errorsS3.length == req.files.length){
+                        res.status(500).json({ "message" : 'Error occured while trying to upload to S3 bucket'});
+                    }
                 }
-
+                
+                fs.unlinkSync(fileSingle.path); //delete the image on server after uploading
+                
                 if (data) {
-                    console.log(data);
-                    fs.unlinkSync(req.file.path); // Empty temp folder after we upload all images to s3
-                    const locationUrl = data.Location;
+                    let locationUrl = data.Location;
+                    uploadData.push(data);
                     Product.create({ 
                             userId: req.body.userId,
                             name: req.body.name,
                             productImageURL: locationUrl 
-                    })
-                    .then(product => {
-                        res.json({ message: 'Product created successfully', product });
-                    })
-                    .catch(err => {
-                        console.log('Error occured while trying to save to DB', err);
+                    }).catch(err => {
+                        errors.push(err);
                     });
+                    
+                    //return a message to client after the upload finished
+                    if(req.files.length == uploadData.length ){
+                        res.status(201).json({ "message" : 'Products uploaded successfully'});
+                    }
+
+                    //return a message to client if save to db failed
+                    if((req.files.length == uploadData.length) && errors.length > 0){
+                        res.status(500).json({ "message" : 'Error occured while trying to save to DB'});
+                    }
                 }
             });
+        }).catch(e => {
+            res.status(500).json({ "message" : 'Error occured while upload the file, this might be the server side issue!'});
         })
+    })
 };
 
 //following part is for the actual api
@@ -86,8 +121,10 @@ router
                 callback(null, true);
             } 
         })
-        .single('image'),
+        .array('image'),
         productUpload
   )
 
+
+//delete one or more selected image
 module.exports = router;
